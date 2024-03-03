c3e064d46985f76e3f4b1bba6aaad954849cef00
Eduardo Gonelli <37277530+Eduardo-Gonelli@users.noreply.github.com>
2024-03-02 22:59:20 -0300

    Update Poog

--- Assets/MainCharacter.cs
+++ Assets/MainCharacter.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class MainCharacter : MonoBehaviour
 {
+    public float speed = 5.0f;
+    private Rigidbody2D rb;
+    [HideInInspector] public bool isGrounded;
+    [SerializeField] private Transform shotSpawn;
+
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
