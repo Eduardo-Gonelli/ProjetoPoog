2a006eac6e8e15657c20e51a5285358c9bf64c5b
Eduardo Gonelli <37277530+Eduardo-Gonelli@users.noreply.github.com>
2024-03-07 21:31:35 -0300

    Update Reorganização dos códigos

--- Assets/Scripts/PlayerBase.cs
+++ Assets/Scripts/PlayerBase.cs
@@ -2,37 +2,65 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Enum serve como um seletor
+// onde só pode ser escolhido um valor
+// de cada vez
+public enum MoveType
+{
+    Transform,
+    Translate,
+    Rigidbody2D
+}
+
 public class PlayerBase : MonoBehaviour
 {
+    // Variável para controlar a vida do personagem
     const int MAX_HEALTH = 100;
     private int health;
-    [SerializeField] float speed = 5.0f;
     
-    //Vector3 inputVector;
+    // Variável para controlar o tipo de movimento
+    public MoveType moveType = MoveType.Translate;
+    [SerializeField] float speed = 5.0f;
+    Vector3 inputVector;
     Rigidbody2D rb2d;
    
     void Start()
     {
+        // inicializa a vida do personagem com o valor máximo
         health = MAX_HEALTH;
+        // Inicializa o componente Rigidbody2D
         rb2d = GetComponent<Rigidbody2D>();
     }
-
-
-
-    // Update is called once per frame
+    
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        // inputVector = new Vector3(horizontalInput, verticalInput, 0);
-        MoveByTransformTranslate(horizontalInput, verticalInput);
-        //MoveByTransform(horizontalInput, verticalInput);
-        if(Input.GetKeyDown(KeyCode.Space))
+        // Recebe o input do jogador
+        float horizontalInput = Input.GetAxis("Horizontal");        
+        // Cria um vetor com os valores do input
+        inputVector = new Vector3(horizontalInput, 0, 0);
+        // Chama o método de movimento de acordo com o tipo selecionado
+        // No Update chama os tipos não físicos
+        switch(moveType)
         {
-            Jump();
+            case MoveType.Transform:
+                MoveByTransform();
+                break;
+            case MoveType.Translate:
+                MoveByTransformTranslate();
+                break;
+        }        
+    }
+
+    private void FixedUpdate()
+    {
+        // No FixedUpdate chama os tipos físicos
+        if (moveType == MoveType.Rigidbody2D)
+        {
+            MoveByRigidbody2dForce();
         }
     }
 
+    // Método para receber dano
     public void TakeDamage(int damage)
     {
         health -= damage;
@@ -41,32 +69,30 @@ public class PlayerBase : MonoBehaviour
             // Die();
         }
     }
+
+
+
+
+    #region Metodos de Movimento
     // Método para mover o personagem modificando a posição diretamente
-    void MoveByTransform(float horizontalInput, float verticalInput)
-    {
-        // Removendo a gravidade do personagem podemos experimentar o verticalInput
-        Vector3 newPosition = transform.position + new Vector3(horizontalInput, verticalInput, 0) * speed * Time.deltaTime;
+    void MoveByTransform()
+    {        
+        Vector3 newPosition = transform.position + inputVector * speed * Time.deltaTime;
         transform.position = newPosition;
     }
+
     // Método para mover o personagem usando o método Translate
-    void MoveByTransformTranslate(float horizontalInput, float verticalInput)
+    void MoveByTransformTranslate()
     {
-        Vector3 movement = new Vector3(horizontalInput, verticalInput, 0) * speed * Time.deltaTime;
+        Vector3 movement = inputVector * speed * Time.deltaTime;
         transform.Translate(movement);
     }
-    private void FixedUpdate()
-    {
-        //MoveByRigidbody2dForce();
-    }
-    //// Método para mover o personagem usando a força do Rigidbody2D
-    //void MoveByRigidbody2dForce()
-    //{
-    //    Vector3 forceVector = inputVector * speed;
-    //    rb2d.AddForce(forceVector);
-    //}
 
-    void Jump()
+    // Método para mover o personagem usando a força do Rigidbody2D
+    void MoveByRigidbody2dForce()
     {
-        // Código para fazer o personagem pular
+        Vector3 forceVector = inputVector * speed;
+        rb2d.AddForce(forceVector);
     }
+    #endregion
 }
--- Assets/Scripts/PlayerJump.cs
+++ Assets/Scripts/PlayerJump.cs
@@ -4,8 +4,12 @@ public class PlayerJump : MonoBehaviour
 {
     public float jumpForce = 700f;
     private Rigidbody2D rb;
+    // Flag para indicar se o personagem está no chão
     private bool isGrounded = true;
-    private bool jumpRequested = false; // Flag para indicar se o pulo foi solicitado
+    // Flag para indicar se o pulo foi solicitado
+    private bool jumpRequested = false; 
+    // Flag para indicar se o personagem pode realizar um segundo pulo
+    bool doubleJump = true;
 
     void Start()
     {
@@ -15,9 +19,26 @@ public class PlayerJump : MonoBehaviour
     void Update()
     {
         // Captura a solicitação de pulo
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (Input.GetButtonDown("Jump"))
         {
-            jumpRequested = true;
+            // Verifica se o personagem está no chão
+            // Se sim, solicita o pulo
+            if(isGrounded)
+            {
+                jumpRequested = true;
+            }
+            // Se não, verifica se o personagem pode realizar um segundo pulo
+            else if (doubleJump)
+            {
+                // Ao realizar um segundo pulo, as forças do Rigidbody2D
+                // geralmente são somadas, o que pode resultar
+                // em um pulo muito alto. Para evitar isso,
+                // a velocidade do personagem é zerada antes
+                rb.velocity = Vector2.zero;
+                jumpRequested = true;
+                doubleJump = false;
+            }
+            
         }
     }
 
@@ -27,16 +48,19 @@ public class PlayerJump : MonoBehaviour
         if (jumpRequested)
         {
             rb.AddForce(new Vector2(0, jumpForce));
-            isGrounded = false; // Supõe-se que o personagem não esteja mais no chão
+            isGrounded = false; // Ao pular o personagem não está mais no chão
             jumpRequested = false; // Reseta a solicitação de pulo
         }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
+        // Verifica se o personagem está no chão
         if(collision.gameObject.CompareTag("Ground"))
         {
+            // Se sim, permite pular novamente
             isGrounded = true;
+            doubleJump = true;
         }
     }
 }
