301ed6c99cb81c1519ed6477e99643b5e46d7174
Eduardo Gonelli <37277530+Eduardo-Gonelli@users.noreply.github.com>
2024-03-11 21:51:58 -0300

    Update CameraController.cs

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,41 +1,66 @@
 /// <summary>
-/// Exemplo de c‚mera extraÌdo do tutorial:
+/// Exemplo de c√¢mera extra√≠do do tutorial:
 /// https://www.codemahal.com/making-the-camera-follow-the-player-2d-unity
-/// Adaptado por Eduardo Gonelli para permitir suavizaÁ„o no eixo Y
+/// Adaptado por Eduardo Gonelli para permitir suaviza√ß√£o no eixo Y
 /// </summary>
 
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public GameObject player;       // ReferÍncia ao jogador - associar via Inspector
+    public GameObject player;       // Refer√™ncia ao jogador - associar via Inspector
     public float offsetX;           // Offset no eixo X
     public float offsetY;           // Offset no eixo Y
-    public float offsetSmoothingX;  // SuavizaÁ„o no eixo X
-    public float offsetSmoothingY;  // SuavizaÁ„o no eixo Y
-    private Vector3 playerPosition; // PosiÁ„o do jogador
+    public float offsetSmoothingX;  // Suaviza√ß√£o no eixo X
+    public float offsetSmoothingY;  // Suaviza√ß√£o no eixo Y
+    private Vector3 playerPosition; // Posi√ß√£o do jogador
 
     void Update()
     {
-        // PosiÁ„o inicial do jogador combinada com os offsets e com a c‚mera
+        // Posi√ß√£o inicial do jogador combinada com os offsets e com a c√¢mera
         playerPosition = new Vector3(player.transform.position.x, player.transform.position.y + offsetY, transform.position.z);
 
-        // Se o jogador estiver virado para a direita, o offset È positivo
+        // Se o jogador estiver virado para a direita, o offset √© positivo
         if (player.transform.localScale.x > 0f)
         {
             playerPosition = new Vector3(playerPosition.x + offsetX, playerPosition.y, playerPosition.z);
         }
-        // Se o jogador estiver virado para a esquerda, o offset È negativo
+        // Se o jogador estiver virado para a esquerda, o offset √© negativo
         else
         {
             playerPosition = new Vector3(playerPosition.x - offsetX, playerPosition.y, playerPosition.z);
         }
 
-        // SuavizaÁ„o separada para os eixos X e Y
+        // Suaviza√ß√£o separada para os eixos X e Y
         Vector3 newPosition = Vector3.Lerp(transform.position, playerPosition, offsetSmoothingX * Time.deltaTime);
         newPosition.y = Mathf.Lerp(transform.position.y, playerPosition.y, offsetSmoothingY * Time.deltaTime);
         transform.position = newPosition;
+    }
+
+    public void ZoomIn()
+    {
+        StartCoroutine(Zoom(10, 1));
+    }
+
+    public void ZoomOut()
+    {
+        StartCoroutine(Zoom(5, 1));
+    }
+
+    IEnumerator Zoom(float size, float duration)
+    {
+        float time = 0;
+        float orinalSize = Camera.main.orthographicSize;
+        float newSize = size;
+        while (time < duration)
+        {
+            Camera.main.orthographicSize = Mathf.Lerp(orinalSize, newSize, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        Camera.main.orthographicSize = newSize;
     }
 }
