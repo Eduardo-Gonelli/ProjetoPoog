8a7fe7b91c0d6430cfb623bc5898f060fa5b5af7
Eduardo Gonelli <37277530+Eduardo-Gonelli@users.noreply.github.com>
2024-03-29 10:29:09 -0300

    Update JumpThrough.cs

--- Assets/Scripts/JumpThrough.cs
+++ Assets/Scripts/JumpThrough.cs
@@ -4,31 +4,15 @@ using UnityEngine;
 
 public class JumpThrough : MonoBehaviour
 {
-    private GameObject player;
-    private float halfY;
-    private float playerHalfY;
-    Collider2D colliderBox;
-    Collider2D playerCollider;
-    // Start is called before the first frame update
+
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
-        playerCollider = player.GetComponent<Collider2D>();
-        colliderBox = GetComponent<BoxCollider2D>();
-        halfY = GetComponent<BoxCollider2D>().size.y / 2;
-        playerHalfY = playerCollider.bounds.size.y / 2;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(player.transform.position.y - playerHalfY < transform.position.y + halfY && colliderBox.isTrigger == false)
-        {
-            colliderBox.isTrigger = true;
-        }
-        else if (player.transform.position.y - playerHalfY > transform.position.y + halfY && colliderBox.isTrigger == true)
-        {
-            colliderBox.isTrigger = false;
-        }
+
     }
 }
